2834c68be94d5a30d190accbb19a11b56b4cda88
Cory Marshman <corymarshman@Corys-MacBook-Air.local>
2020-01-25 15:31:48 -0500

    css and bootstraps additions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO burgers (name, devoured) VALUES ("Chicken Avacaod Burger"); 
+INSERT INTO burgers (name, devoured) VALUES ("Chicken Avacaod Burger", false); 
 INSERT INTO burgers (name, devoured) VALUES ("Bacon wrapped Jalapeño burger", true);
-INSERT INTO burgers (name, devoured) VALUES ("Beyond Beef Burger");
+INSERT INTO burgers (name, devoured) VALUES ("Beyond Beef Burger", false);
 INSERT INTO burgers (name, devoured) VALUES ("Gardenburger Black Been Chipotle Burger", true);
-INSERT INTO burgers (name, devoured) VALUES ("Boca Vegan Burger");
+INSERT INTO burgers (name, devoured) VALUES ("Boca Vegan Burger", false);
 
 
 
