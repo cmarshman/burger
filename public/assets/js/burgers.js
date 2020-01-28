9875930fe885d08e86ae5bbde8c7a9708d00ca66
Cory Marshman <corymarshman@Corys-Air.attlocal.net>
2020-01-27 23:40:50 -0500

    Design components added

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,3 +1,13 @@
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if( target.length ) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+          scrollTop: target.offset().top
+      }, 1000);
+  }
+});
+
 $(function() {
     $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
